c6cf8d5cc95830be5eb4eaeefeaa6ca402945a0d
Winston Smith <wsbraves@gmail.com>
2020-09-30 19:09:16 -0400

    Updates

---
+++ precache-manifest.6bb713eecaf68b88a60cdbd711ac383e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "641ad920307dd10f87d7e9cd3800b209",
+    "url": "/react-movie-search/index.html"
+  },
+  {
+    "revision": "273031a3a1f4e1a0f92b",
+    "url": "/react-movie-search/static/css/main.3a25f99e.chunk.css"
+  },
+  {
+    "revision": "b4a9dc5e4849e37906ee",
+    "url": "/react-movie-search/static/js/2.b3450f78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-movie-search/static/js/2.b3450f78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "273031a3a1f4e1a0f92b",
+    "url": "/react-movie-search/static/js/main.195835a9.chunk.js"
+  },
+  {
+    "revision": "3a697912e429c397a3a1",
+    "url": "/react-movie-search/static/js/runtime-main.37af514e.js"
+  }
+]);
